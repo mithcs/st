dfeadf454b9b08a4f3c77e7193d0031a82156182
Mitesh Soni <smiteshhc@gmail.com>
2025-07-13 12:29:10 +0530

    Updated Font to BlexMonoNerdFont

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "ComicShannsMonoNerdFont:pixelsize=18:antialias=true";
+static char *font = "BlexMonoNerdFont:pixelsize=18:antialias=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
